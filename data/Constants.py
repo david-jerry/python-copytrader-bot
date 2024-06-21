d35d3943198b48170e41ae272f9aa89542da446e
Jeremiah David <jeremiahedavid@gmail.com>
2024-06-21 02:11:50 +0100

    install requirements packages

--- data/Constants.py
+++ data/Constants.py
@@ -22,3 +22,28 @@ This is a bot that helps you to copy trade whales, swap tokens, and more.
 <b>Disclaimer:</b>
 This bot is for informational purposes only and should not be considered financial advice.
 """
+
+about_message = """<b>Welcome to CopiTradaBot!</b>
+
+We're your one-stop shop for all your crypto trading needs.
+
+<b>What We Offer:</b>
+
+* <b>Effortless Trading:</b> Buy and sell crypto assets directly within the Telegram interface.
+* <b>Seamless Swapping:</b> Exchange tokens for one another with ease.
+* <b>Automated Strategies:</b> Leverage copy trading to replicate successful traders' strategies.
+* <b>Smart Wallet Management:</b> Generate new wallet addresses or connect your existing ones for convenient access.
+
+<b>Benefits:</b>
+
+* <b>Simplified Transactions:</b> Streamline your crypto trading experience.
+* <b>Data-Driven Decisions:</b> Copy trading helps you capitalize on profitable strategies.
+* <b>Enhanced Security:</b> Manage your crypto assets within a secure platform.
+
+<b> CopiTradaBot, Get Started Today!</b>
+
+1. Start a chat with [@CopiTradaBot].
+2. Follow the on-screen instructions to connect your wallet.
+
+Stay tuned for exciting updates!
+"""
